d612e7172c07d211a9b454d6e56ef26acc93de5b
YACOUBI-Abdelhakime <hakim.199911@gmail.com>
2024-09-10 23:39:53 +0200

    Enable CORS form my client app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // Enable CORS for my client app
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
